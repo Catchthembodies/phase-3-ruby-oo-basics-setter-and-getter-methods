e3f06ecb70f847a395befcedd4ccfae3c6fa3a0a
Catchthembodies <103537060+Catchthembodies@users.noreply.github.com>
2022-07-01 00:54:36 -0700

    commit tree 4340921a42a084c8883de1bec1ae0ff36f560dd0

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+        # getter method
+        def name
+          @name
+        end
+              # setter method
+        def name=(name)
+          @name = name
+        end
 
+        def job 
+            @job
+        end
+
+        def job=(job)
+            @job = job
+        end
 end
